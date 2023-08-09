2388fdf8166329535adfef24589d7d56825f40bd
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-08-09 18:34:02 +0300

    moved apikey to .env

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
+import os
 import pathlib
+
+import dotenv
+
+dotenv.load_dotenv()
 
 ROOT_FOLDER = pathlib.Path(__file__).parent
 APIKEY_PATH = pathlib.Path(ROOT_FOLDER, 'apikeys.json')
 
-SCRAPINGBEE_APIKEY = "M977YHXCMPJJ569DSB0B8KSKL9NRU2O2327MIDT55785T8LS9TJGDW4GFMCMOZNRVN3GPSXF0Y6DGC32"  # move to .env
+SCRAPINGBEE_APIKEY = os.environ['SCRAPINGBEE_APIKEY']
 MONGO_URL = 'mongodb://root:example@10.107.8.10:27017'  # move to .env
