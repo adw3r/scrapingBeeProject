1b43c950c9db3f6547ec62d4e279bbf19a7ff576
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-09-20 17:05:14 +0300

    main.py moved from app to root folder verify=False removed from scrapingbee request

--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,9 @@ import pathlib
 from random import choice
 from string import digits, ascii_letters
 
+import bs4
+import requests
+
 from app import config
 
 
@@ -23,3 +26,37 @@ def dump_json(data: dict, file_name) -> None:
 def load_json(file_name: str | pathlib.Path) -> dict | list:
     with open(file_name, 'rb') as file:
         return json.load(file)
+
+
+def check_form(url):
+    cookies = {
+        '_ga': 'GA1.2.722049793.1693989861',
+        '_gid': 'GA1.2.1143209261.1693989861',
+        '_gat': '1',
+        '_ga_R1XPVJRD27': 'GS1.2.1693989861.1.1.1693989861.0.0.0',
+    }
+
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        soup = bs4.BeautifulSoup(response.text, 'lxml')
+        form = soup.find('form')
+        if form:
+            inputs = form.find_all('input')
+            input_names = [i.get('name') for i in inputs]
+    except Exception as error:
+        print(error)
--- main.py
+++ main.py
@@ -2,35 +2,21 @@ import webbrowser
 
 from app import scrapingbee, database
 
-searching_statement = ('inurl:'
-                       'recommendation'
-                       ' intext:'
-                       '"friend\'s email"'
-                       '')
 
-
-def parse_donors():
-    searching_query = scrapingbee.SearchingQuery(
-        **dict(
-            search=searching_statement,
-            page=1,
-            nb_results=100,
-            # country_code='uk'
-        )
-    )
+def parse_donors(searching_query: scrapingbee.SearchingQuery):
     scraping_object: scrapingbee.ScrapingObject = scrapingbee.send_request(searching_query)
     for res in scraping_object.organic_results:
         res.searching_query = searching_query
         database.OrganicResultsRepo.save_one(res)
 
 
-def main():
-    stmnt = {'searching_query.search': {'$regex': searching_statement}, 'status': 'not viewed'}
+def find_donors(stmnt: dict):
     results = database.OrganicResultsRepo.collection.find(stmnt, {})
     c = 0
     for result in results:
-        print(result['url'])
-        webbrowser.open(result['url'])
+        url = result['url']
+        webbrowser.open(url)
+
         result['status'] = 'viewed'
         database.OrganicResultsRepo.collection.update_one({'_id': result['_id']}, {'$set': result})
         c += 1
@@ -39,6 +25,20 @@ def main():
             c = 0
 
 
+def main():
+    searching_statement = 'inurl:EmailToFriend intext:"friend\'s email"'
+    searching_query = scrapingbee.SearchingQuery(
+        **dict(
+            search=searching_statement,
+            page=1,
+            nb_results=100,
+            # country_code='uk'
+        )
+    )
+    stmnt = {'searching_query.search': {'$regex': searching_statement}, 'status': 'not viewed'}
+    parse_donors(searching_query)
+    find_donors(stmnt)
+
+
 if __name__ == '__main__':
-    parse_donors()
     main()
